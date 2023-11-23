3fed9a889bbefa6e792b1cdf36b6dc3b6f03d9ef
programehr <mehrin_saremi@yahoo.com>
2023-11-23 21:48:51 +0400

    modify to work with models without batchnorm

--- CLP/defense.py
+++ CLP/defense.py
@@ -11,26 +11,34 @@ import torch.nn as nn
 
 def CLP(net, u):
     params = net.state_dict()
-    for name, m in net.named_modules():
+    modules = list(net.named_modules())
+    for module_ix, (name, m) in enumerate(modules):
+        next_mod = modules[module_ix+1] if module_ix <= len(modules)-2 else None
+        if isinstance(next_mod, nn.BatchNorm2d):
+            conv = m
+            continue
         if isinstance(m, nn.BatchNorm2d):
             std = m.running_var.sqrt()
-            weight = m.weight
-
-            channel_lips = []
-            for idx in range(weight.shape[0]):
-                # Combining weights of convolutions and BN
-                w = conv.weight[idx].reshape(conv.weight.shape[1], -1) * (weight[idx]/std[idx]).abs()
-                channel_lips.append(torch.svd(w.cpu())[1].max())
-            channel_lips = torch.Tensor(channel_lips)
-
-            index = torch.where(channel_lips>channel_lips.mean() + u*channel_lips.std())[0]
-
-            params[name+'.weight'][index] = 0
-            params[name+'.bias'][index] = 0
-            print(index)
-        
-       # Convolutional layer should be followed by a BN layer by default
         elif isinstance(m, nn.Conv2d):
             conv = m
+        else:
+            continue
+        weight = m.weight
+
+        channel_lips = []
+        for idx in range(weight.shape[0]):
+            # Combining weights of convolutions and BN
+            w = conv.weight[idx].reshape(conv.weight.shape[1], -1)
+            if isinstance(m, nn.BatchNorm2d):
+                w = w * (weight[idx]/std[idx]).abs()
+            channel_lips.append(torch.svd(w.cpu())[1].max())
+        channel_lips = torch.Tensor(channel_lips)
+
+        index = torch.where(channel_lips>channel_lips.mean() + u*channel_lips.std())[0]
+        print(f'module number: {module_ix}, length of index: {len(index)}')
+
+        params[name+'.weight'][index] = 0
+        if name+'.bias' in params:
+            params[name+'.bias'][index] = 0
 
     net.load_state_dict(params)
